c8c4040eae4f215a2673d0ce79ee136a25f4b0ba
Paul Bryan <pbryan@anode.ca>
2018-12-22 23:20:07 -0800

    Add Python structure.

---
+++ src/montri/__init__.py
@@ -0,0 +1,3 @@
+"""Montri: A radio direction finding monitoring station."""
+
+from ._version import __version__
---
+++ src/montri/_version.py
@@ -0,0 +1,3 @@
+"""Montri: A radio direction finding monitoring station."""
+
+__version__ = "1.0a1"
